c31b620a89c03e75a08997da760084e5a0d5e5c5
Hongjian <hongjian-wang@outlook.com>
2016-09-06 01:09:43 -0400

    Get code in OAuth authorization code flow

--- ActivityTrackerServer.py
+++ ActivityTrackerServer.py
@@ -10,18 +10,53 @@ that are collected by Fitbit.
 """
 
 
-from flask import Flask
-print __name__
+from flask import Flask, session, url_for, redirect, request
+from Oauth2 import oauthServer
+
+
 app = Flask(__name__)
+app.secret_key = "activitytracker"
+
+
+# Go to fitbit.com to authorize user with OAuth, and get access token.
+fitbit = oauthServer()
 
 
 @app.route("/")
 def index():
-    return "Hello! This is ActivityTracker server."
+    if "fitbit_token" in session:
+        return "Hello! Fitbit user."
+    else:
+        return redirect(url_for('login'))
+        
+        
+@app.route("/login")
+def login():
+    print "call fitbit.com authorizer"
+    return fitbit.authorize(callback="http://98.235.161.247:9292/oauth_accept")
     
+    
+@app.route("/oauth_accept")
+def oauthAccept():
+    res = request.args.get('code')
+    if res is None:
+        return "Fitbit response is None"
+    else:
+        session['twitter_token'] = (
+            res        
+        )
+        return res
+        
+
+@fitbit.tokengetter
+def get_fitbit_token(token=None):
+    return session.get('fitbit_token')
+    
+    
+
 
 if __name__ == '__main__':
     # The server is running on my local port 192.168.0.6:8080, which is behind
     # my router. Port forwarding has been setup, so that this server is accessible
     # through public IP 98.235.161.247:9292 
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8080, debug=True)
---
+++ Oauth2.py
@@ -0,0 +1,62 @@
+"""
+Hongjian
+9/5/2016
+
+Oauth authentication of AT server.
+
+Use the online registered Oauth2 client ID and secret to authenticate AT server
+with the Fitbit Oauth server.
+"""
+
+import unittest
+from flask_oauthlib.client import OAuth
+
+
+def getOauthCredentials():
+    """
+    Get Oauth credentials from local config file
+    """
+    credentials = {}
+    
+    with open("oauth-credentials", "r") as fin:
+        credentials['clientId'] = fin.readline().strip()
+        credentials['clientSecret'] = fin.readline().strip()
+    
+    return credentials
+
+
+def oauthServer():
+    credentials = getOauthCredentials()
+    
+    oauth = OAuth()
+    Fitbit = oauth.remote_app(
+        name='fitbit',
+        consumer_key=credentials['clientId'],
+        consumer_secret=credentials['clientSecret'],
+        access_token_method='POST',
+        access_token_url='https://api.fitbit.com/oauth2/token',
+        request_token_params={'scope':'activity heartrate',
+                              'expires_in': 2592000},
+        authorize_url='https://www.fitbit.com/oauth2/authorize'
+        )
+    return Fitbit
+
+
+
+
+class Oauth2Test(unittest.TestCase):
+
+    def test_getOauthCredentials(self):
+        res = getOauthCredentials()
+        assert len(res['clientId']) > 3
+        assert len(res['clientSecret']) > 10
+        
+        
+    def test_oauthServer(self):
+        Fitbit = oauthServer()
+        assert Fitbit.name == 'fitbit'
+        
+        
+
+if __name__ == '__main__':
+    unittest.main()
